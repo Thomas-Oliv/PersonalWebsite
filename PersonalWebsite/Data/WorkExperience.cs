be793991b0b304c5063cf4f8efe938aa64817f0b
Thomas <drivemenuts766@gmail.com>
2021-01-14 14:18:25 -0500

    Add project files.

---
+++ PersonalWebsite/Data/WorkExperience.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Data
+{
+    public class WorkExperience
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public string Employer { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ PersonalWebsite/Pages/About.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using PersonalWebsite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace PersonalWebsite.Pages
+{
+    public partial class About
+    {
+        List<WorkExperience> experiences { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            string query = "SELECT * FROM WorkExperience";
+            experiences = await _data.LoadData<WorkExperience, dynamic>(query, new { }, _config.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+}
