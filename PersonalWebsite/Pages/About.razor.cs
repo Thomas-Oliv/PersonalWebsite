72cddc755464c501fca3cb4a799a50df00c37dc7
Thomas <drivemenuts766@gmail.com>
2021-01-17 00:41:47 -0500

    Added DB Integration for About Page

--- PersonalWebsite/Pages/About.razor.cs
+++ PersonalWebsite/Pages/About.razor.cs
@@ -15,5 +15,13 @@ namespace PersonalWebsite.Pages
             Primary = "#1565c0",
             Secondary = "#0d47a1"
         };
+        private List<WorkExperience> experiences;
+        private List<WorkExperienceHead> head;
+
+        protected override async Task OnInitializedAsync()
+        {
+            experiences = await _data.LoadData<WorkExperience, dynamic>("SELECT * FROM WorkExperience", new { }, _config.GetConnectionString("DefaultConnection"));
+            head = await _data.LoadData<WorkExperienceHead, dynamic>("SELECT * FROM WorkExperienceHead", new { }, _config.GetConnectionString("DefaultConnection"));
+        }
     }
 }
